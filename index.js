de06114f77eb1046ef3b5ec5255c4ba2a4f90ac5
Travis <travisechapman@gmail.com>
2016-08-21 16:14:47 -0400

    Test twilio respomse

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const express = require('express');
+const express = require('express'),
+      twilio = require('twilio');
 
 const cfg = require('./config');
 
@@ -8,6 +9,15 @@ const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello, world!');
+});
+
+app.post('/sms', function (req, res) {
+  var twiml = twilio.twimlResponse();
+  twiml.say('Thanks for texting!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+
 });
 
 app.listen(cfg.PORT, function () {
