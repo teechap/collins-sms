bf1fa7b952575af28b816b2be28cbcdfdc1818c1
Travis <travisechapman@gmail.com>
2016-08-28 12:54:08 -0400

    Use twilio.validateExpressRequest

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ app.get('/', function (req, res) {
   res.send('Hello, world!');
 });
 
-app.post('/sms', twilio.webhook(), function (req, res) {
-  // webhook() validates that the POST request is really from Twilio
-  // and autmagically handles twiml objects passed to res.send().
-  
-  var twiml = new twilio.TwimlResponse();
-  twiml.message("You validated the Twilio message, congrats.");
-  res.send(twiml);
+app.post('/sms', function (req, res) {
+
+  if (twilio.validateExpressRequest(req, cfg.TWILIO_AUTH_TOKEN)) {
+
+    var twiml = new twilio.TwimlResponse();
+    twiml.message("You validated the Twilio message, congrats.");
+    res.end(twiml.toString());
+
+  } else {
+    res.status(403).send("POST request was not authenticated from Twilio.");
+  }
 });
 
 app.listen(cfg.PORT, function () {
