df831958216a3df1f0f08d15864c5e5a30702684
Travis <travisechapman@gmail.com>
2016-08-28 12:27:14 -0400

    Added twilio.webhook() to validate whether POST to /sms is actually from Twilio

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 'use strict';
 
 const express = require('express'),
+      bodyParser = require('body-parser'),
       twilio = require('twilio');
 
 const cfg = require('./config');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get('/', function (req, res) {
   res.send('Hello, world!');
 });
 
-app.post('/sms', function (req, res) {
+app.post('/sms', twilio.webhook(), function (req, res) {
+  // webhook() validates that the POST request is really from Twilio
+  // and autmagically handles twiml objects passed to res.send().
   var twiml = new twilio.TwimlResponse();
-  twiml.message("I'm a robot. Stop texting me, Kady.");
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-
+  twiml.message("You validated the Twilio message, congrats.");
+  res.send(twiml);
 });
 
 app.listen(cfg.PORT, function () {
